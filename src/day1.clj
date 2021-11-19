60d571e5f12b34d5138176ad9c75692a974e7685
NegatioN <joakimrishaug@gmail.com>
2021-11-19 19:12:01 +0100

    just do some clojure

--- src/day1.clj
+++ src/day1.clj
@@ -3,11 +3,11 @@
       [clojure.string :as str]))
 (def myData [1721 979 366 299 675 1456])
 
-(for [i myData
-      j myData
-      :when (= 2020 (+ i j))]
-  (* i j) )
-
+(defn find-and-mul [target data] (for [i data j data
+                                        :when (= target (+ i j))]
+                                    (* i j) )
+                                  )
+(find-and-mul 2020 myData)
 
 (for [i myData
       j myData
