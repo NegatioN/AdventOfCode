e8294ba5cae87b07a9ee3ee1341e5ead7cdf5a3e
NegatioN <joakimrishaug@gmail.com>
2021-12-02 18:21:05 +0100

    p2 day3 julia

--- 2020/day3/day3.jl
+++ 2020/day3/day3.jl
@@ -1,12 +1,19 @@
 filepath = "../data/day3"
 
-treehits = 0
-x = 0
-for l in readlines(filepath)
-    mapLine = Int64[x == '#' ? 1 : 0 for x in l]
-    global treehits += mapLine[(x % length(mapLine)) + 1]
-    global x += 3
+function checkHits(xStep::Int, yStep::Int, textlines::Array)
+    treehits, x = 0, 0
+    for l in textlines[1:yStep:end]
+        mapLine = Int64[x == '#' ? 1 : 0 for x in l]
+        treehits += mapLine[(x % length(mapLine)) + 1]
+        x += xStep
+    end
+    treehits
 end
-
-println(treehits)
-
+textlines = readlines(filepath)
+p1 = checkHits(3, 1, textlines)
+p2 = 1
+for (x, y) in zip([1, 3, 5, 7, 1], [1,1,1,1,2])
+   global p2 *= checkHits(x, y, textlines)
+end
+println(p1)
+println(p2)
