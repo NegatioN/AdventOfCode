f2155fd94b2a5d75e72ec874d450023df1a8903d
NegatioN <joakimrishaug@gmail.com>
2021-12-04 21:59:04 +0100

    day 4 julia

---
+++ 2021/day4.jl
@@ -0,0 +1,37 @@
+textlines = readlines("data/day5")
+
+numbers = map(x->parse(Int, x),split(textlines[1], ','))
+
+boardData = textlines[3:end]
+boards = zeros(Int8, 5, 5, Int(ceil(length(boardData) / 6)))
+
+for (i, l) in enumerate(boardData)
+    if l != ""
+        for (j, c) in enumerate(split(l))
+            boards[i%6, j, Int(floor(i/6))+1] = parse(Int8, c)
+        end
+    end
+end
+#display(boards)
+
+function scoreBoards(boards, numbers)
+    boardHits = zeros(Int8, size(boards))
+    boardsInPlay = 1:size(boards)[end]
+    scoreboard = []
+    for n in numbers
+        boardHits[boards .== n] .= 1
+        vertiHits, horizHits = sum(boardHits, dims=1), sum(boardHits, dims=2)
+        removed = []
+        for b in boardsInPlay
+            if maximum(vertiHits[:, :, b]) >= 5 || maximum(horizHits[:,:,b]) >= 5
+                push!(scoreboard, sum(boards[:,:,b][boardHits[:, :, b] .== 0]) * n)
+                push!(removed, b)
+            end
+        end
+        boardsInPlay = setdiff(boardsInPlay, removed)
+    end
+    scoreboard
+end
+
+score = scoreBoards(boards, numbers)
+println(score)
