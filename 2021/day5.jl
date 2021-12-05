bf473cfb208343a1569d744aedcd7fe327b583ef
NegatioN <joakimrishaug@gmail.com>
2021-12-05 15:46:35 +0100

    Steal a nicer way to parse

--- 2021/day5.jl
+++ 2021/day5.jl
@@ -1,18 +1,16 @@
 function overlappingPoints(textlines::Array, mat::Matrix, diagonalfunc::Function)
    for l in textlines
-       p1, p2 = map(z->split(z, ","), split(l, " -> "))
-       (x1, y1), (x2, y2) = [parse(Int, x) for x in p1], [parse(Int, x) for x in p2]
+       (x1, y1, x2, y2) = map.(x->parse(Int,x)+1, split.(replace(l, " -> " => ","), ","))
        xstep, ystep = x1 > x2 ? -1 : 1, y1 > y2 ? -1 : 1
        f(x, y) = x1 == x2 || y1 == y2 ? collect(Base.Iterators.product(x, y)) : diagonalfunc(x, y)
        for (i, j) in f(x1:xstep:x2, y1:ystep:y2)
-            mat[j+1, i+1] += 1
+            mat[j, i] += 1
        end
    end
    sum(mat .> 1)
 end
 
-mat = zeros(Int8, (1000, 1000))
-textlines = readlines("data/day5")
+textlines = readlines("data/day5test")
 empty(x...) = zip([], x...)
-println(overlappingPoints(textlines, mat, empty)) #part1
-println(overlappingPoints(textlines, mat, zip)) #part2
+println(overlappingPoints(textlines, zeros(Int8, (1000, 1000)), empty)) #part1
+println(overlappingPoints(textlines, zeros(Int8, (1000, 1000)), zip)) #part2
