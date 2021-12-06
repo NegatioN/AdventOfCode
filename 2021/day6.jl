7e67b23ba364c8996ca4885d50bb7c07969a8f39
NegatioN <joakimrishaug@gmail.com>
2021-12-06 17:38:52 +0100

    use only one array day6

--- 2021/day6.jl
+++ 2021/day6.jl
@@ -1,15 +1,15 @@
-jellies = map(x->[parse(Int, x), 6], split.(readlines("data/day6")[1], ","))
+fish = map(x->[parse(Int, x), 6], split.(readlines("data/day6")[1], ","))
 
-function calculateSpawn(jellies::Array, i::Int)
-    gen1bins, bins = zeros(Int64, 9), zeros(Int64, 7)
-    for j in jellies gen1bins[j[1]+1] += 1; end
+function calculateSpawn(fish::Array, i::Int)
+    bins = zeros(Int64, 9)
+    for j in fish bins[j[1]+1] += 1; end
     for _ in 1:i
-        spawn = gen1bins[1] + bins[1]
-        bins[1:end-1], gen1bins[1:end-1] = bins[2:end], gen1bins[2:end]
-        bins[end], gen1bins[end] = spawn, spawn
+        spawn = bins[1]
+        bins[1:end-1], bins[end] = bins[2:end], spawn
+        bins[7] += spawn
     end
-    sum(gen1bins) + sum(bins)
+    sum(bins)
 end
 
-println(calculateSpawn(jellies, 80))
-println(calculateSpawn(jellies, 256))
+println(calculateSpawn(fish, 80))
+println(calculateSpawn(fish, 256))
