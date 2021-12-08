f9ed0ccf71ff4ad1a9ff2cf2a0cadea802a23c34
NegatioN <joakimrishaug@gmail.com>
2021-12-08 21:21:09 +0100

    Done

--- 2021/day8.jl
+++ 2021/day8.jl
@@ -1,6 +1,6 @@
 using StatsBase
 
-data = split.(readlines("data/day8test"), " | ")
+data = split.(readlines("data/day8"), " | ")
 function part1(data::Array, targets=[2 3 4 7])
     c = 0
     for (_, out) in data
@@ -35,10 +35,9 @@ function stoi(t::String)
 end
 
 function buildmap(seq)
-    mapping = Dict()
+    mapping = Dict{Char, Char}()
     subseq = split(seq, " ")
     c = countmap(join(subseq))
-    # counts for initial setup, max=10
     # a=8,c=8, b=6, d=7,g=7, e=4, f=9
     # we can always know what maps to e, f, b. Because they have unique counts
     for (k,v) in c
@@ -50,21 +49,11 @@ function buildmap(seq)
             mapping['f'] = k
         end
     end
-    # length 6 == zero, six, nine, only zero has 'd' unset. Which means we can find both g and d?
-    # the letter which has 7 count overall, but only 2 counts in the group of length==6 strings? zero, six and nine
-    d_candidates = collect(keys(c))[values(c) .== 7]
+    mapping['c'] = setdiff(String(filter(x->length(x) == 2, subseq)[1]), String([mapping['f']]))[1]
+    mapping['d'] = setdiff(filter(x->length(x) == 4, subseq)[1], values(mapping))[1]
+    mapping['a'] = setdiff(filter(x->length(x) == 3, subseq)[1], values(mapping))[1]
+    mapping['g'] = collect(setdiff("abcdefg", String(collect(values(mapping)))))[1]
 
-    sixletter_words = filter(x->length(x) == 6, subseq)
-    d_check = countmap(join(sixletter_words))
-    mapping['d'], mapping['g'] = d_check[d_candidates[1]] > 2 ? (d_candidates[2], d_candidates[1]) : (d_candidates[1], d_candidates[2])
-
-    num_six = filter(x->mapping['e'] in x, sixletter_words)[1]
-    mapping['c'] = setdiff("abcdefg", num_six)[1]
-    mapping['a'] = filter(x->x != mapping['c'], collect(keys(c))[values(c) .== 8])[1]
-    #println(setdiff("abcdefg", keys(mapping)))
-    println(length(mapping))
-
-    println(sort(mapping))
     Dict(v=>k for (k,v) in mapping)
 end
 
@@ -74,14 +63,9 @@ function part2(data::Array)
         mapping = buildmap(seq)
         mappedtext = join.(sort.(collect.(map.(x->get(mapping, x, x), split(out, " ")))))
         digits = []
-        println(sort(mapping))
-        println(seq)
-        println(out)
         for t in mappedtext
-            println(t)
             push!(digits, stoi(t))
         end
-        println(digits)
         total += parse(Int, join(digits))
     end
     total
