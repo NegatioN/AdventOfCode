1a65d1459b999d9d4370db4b5b4dfc4a9bcc746f
joakim-rishaug <joakim.rishaug@finn.no>
2021-12-13 19:13:16 +0100

    tmp

--- 2021/day13.jl
+++ 2021/day13.jl
@@ -1,20 +1,29 @@
 input = readlines("data/day13")
 folds = split.(replace.(filter(x->occursin("fold along", x), input), "fold along "=>""), "=")
 dots = reverse.(map(x->parse.(Int, x).+1, split.(filter(x->!occursin("fold along", x), input), ",")))
+
 function fold(dots::Array, folds::Array)
     m = Tuple(maximum(reduce(hcat, dots), dims=2))
-    mat = zeros(Int8, m)
+    mat = zeros(Int32, m)
     mat[map(CartesianIndex, map(Tuple, dots))] .= 1
     for (d, ss) in folds
-        s = parse(Int, ss) + 1
+        s = parse(Int, ss)
+        sb, se = (s+2, s+s+1)
+        println(s, " ", sb, " ", se)
         if d == "y"
-            mat = mat[1:s-1, :] + mat[end:-1:s+1, :]
+            emat = mat[end:-1:sb, :]
+            mat = mat[s-size(emat, 1)+1:s, :] + emat
         else
-            mat = mat[:, 1:s-1] + mat[:, end:-1:s+1]
+            emat = mat[:, end:-1:sb]
+            mat = mat[:, s-size(emat, 2)+1:s] + emat
         end
         println(count(x->x>=1, mat))
     end
     mat
 end
 res = fold(dots, folds)
-display(res)
+println(count(x->x>1, res))
+
+println(map(x->x>=1 ? '#' : ' ', res))
+display(res .>=1)
+display(transpose(res .>1))
