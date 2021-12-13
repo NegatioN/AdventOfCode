8ac63b570c60419aa6d536dfad9fcbae5c16a823
NegatioN <joakimrishaug@gmail.com>
2021-12-13 21:21:42 +0100

    Solve with points instead of matrices...

--- 2021/day13.jl
+++ 2021/day13.jl
@@ -1,29 +1,29 @@
 input = readlines("data/day13")
 folds = split.(replace.(filter(x->occursin("fold along", x), input), "fold along "=>""), "=")
-dots = reverse.(map(x->parse.(Int, x).+1, split.(filter(x->!occursin("fold along", x), input), ",")))
+dots = reverse.(map(x->parse.(Int, x), split.(filter(x->!occursin("fold along", x), input), ",")))
 
-function fold(dots::Array, folds::Array)
-    m = Tuple(maximum(reduce(hcat, dots), dims=2))
-    mat = zeros(Int32, m)
-    mat[map(CartesianIndex, map(Tuple, dots))] .= 1
-    for (d, ss) in folds
+function foldpoints(dots::Array, folds::Array)
+   current = Set(copy(dots))
+   for (axis, ss) in folds
         s = parse(Int, ss)
-        sb, se = (s+2, s+s+1)
-        println(s, " ", sb, " ", se)
-        if d == "y"
-            emat = mat[end:-1:sb, :]
-            mat = mat[s-size(emat, 1)+1:s, :] + emat
-        else
-            emat = mat[:, end:-1:sb]
-            mat = mat[:, s-size(emat, 2)+1:s] + emat
-        end
-        println(count(x->x>=1, mat))
-    end
-    mat
+       for p in current
+            if axis == "y" && p[1] > s
+               push!(current, [s-(p[1]-s), p[2]])
+               pop!(current, p)
+            end
+            if axis == "x" && p[2] > s
+               push!(current, [p[1], s-(p[2]-s)])
+               pop!(current, p)
+            end
+       end
+   end
+   current
 end
-res = fold(dots, folds)
-println(count(x->x>1, res))
 
-println(map(x->x>=1 ? '#' : ' ', res))
-display(res .>=1)
-display(transpose(res .>1))
+res = foldpoints(dots, folds)
+println(res)
+m = zeros(Int8, (8, 40))
+for (x,y) in res
+   m[x+1, y+1] = 1
+end
+display(m)
