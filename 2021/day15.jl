a03a174c2c609176c6943188328fd8550c755665
NegatioN <joakimrishaug@gmail.com>
2021-12-15 22:50:21 +0100

    day15

--- 2021/day15.jl
+++ 2021/day15.jl
@@ -1,20 +1,47 @@
-data = reduce(hcat, map(x->parse.(Int32, [y for y in x]), readlines("data/day15test")))
-println(data)
-gcost = zeros(Int32, size(data))
-for ((i, j), (ic, jc)) in zip(Base.Iterators.product(1:10, 1:10), Base.Iterators.product(9:-1:0, 9:-1:0))
-    gcost[i, j] = ic+jc
+data = reduce(hcat, map(x->parse.(Int32, [y for y in x]), readlines("data/day15")))
+
+function reconstruct_path(camefrom::Dict{CartesianIndex, CartesianIndex}, current::CartesianIndex)
+    total_path = [current]
+    while current ∈ keys(camefrom)
+        current = camefrom[current]
+        push!(total_path, current)
+   end
+   total_path
 end
 
-candidates = [CartesianIndex(1, 1)]
-camefrom = Dict{CartesianIndex, CartesianIndex}()
-gscore = Dict{CartesianIndex}
-while !isempty(candidates)
-    c = pop!(candidates) #Todo, pick the cheapest node of the candidates. fscore
-    if c == CartesianIndex(size(data))
-        println("Winner")
-    end
+function astar(data::Matrix)
+    start, goal, indices = CartesianIndex(1,1), CartesianIndex(size(data)), CartesianIndices(size(data))
+    h(x) = sum(Tuple(goal) .- Tuple(x))
+    candidates = [start]
+    camefrom = Dict{CartesianIndex, CartesianIndex}()
+    gscore, fscore = ones(Int32, size(data)) * 16000, ones(Int32, size(data)) * 16000
+    gscore[start], fscore[start] = 0, h(start)
 
-    for c.neighbour()
+    while !isempty(candidates)
+        _, ind = findmin(map(x->fscore[x], candidates))
+        c = popat!(candidates, ind)
+        if c == goal return reconstruct_path(camefrom, c) end
+
+        for n in map(CartesianIndex, [(c[1]+1, c[2]), (c[1]-1, c[2]), (c[1], c[2]+1), (c[1], c[2]-2)])
+            if n ∈ indices tentative_gscore = gscore[c] + data[n]
+                if tentative_gscore < gscore[n]
+                    camefrom[n] = c
+                    gscore[n], fscore[n] = tentative_gscore, tentative_gscore + h(n)
+                    if n ∉ candidates push!(candidates, n) end
+                end
+            end
+        end
     end
-    println(gcost[c], ",", data[c])
 end
+
+function enlarge_map(data::Matrix)
+    datas = [data]
+    for i in 1:4 push!(datas, (datas[i].%9).+1) end
+    datas = [hcat(datas...)]
+    for i in 1:4 push!(datas, (datas[i].%9).+1) end
+    vcat(datas...)
+end
+
+ldata = enlarge_map(data)
+path = astar(ldata)
+println(sum(ldata[path]) - ldata[1, 1])
