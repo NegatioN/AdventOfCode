628dc4a2f9ad8b3bcdd446493ee9a6a5ab93aca0
joakim-rishaug <joakim.rishaug@finn.no>
2021-12-20 16:18:17 +0100

    Allocate less

--- 2021/day20.jl
+++ 2021/day20.jl
@@ -1,32 +1,22 @@
-using Test
-
-f = open("data/day20")
-const algo = map(x->x=='#' ? 1 : 0, collect(readline(f)))
-readline(f)
-
 function stupidpad(x, f, padsize=1)
     r = f(Int, size(x).+2*padsize)
     r[padsize+1:end-padsize, padsize+1:end-padsize] = x
     r
 end
-image = stupidpad(mapreduce(x->map(y->y=='#' ? 1 : 0,collect(x)), hcat, readlines(f)), zeros, 55)
-
-const all_inds = CartesianIndices(image)
-rows(M) = (view(M, i, :) for i in 1:size(M, 1))
 matbin2dec(x) = parse(Int, join(x), base=2)
-
-const selection = CartesianIndices((-1:1, -1:1))
-function enhance(image, missing_val)
-    new_image = zeros(Int, size(image))
-    for ind in all_inds
-         index = matbin2dec(map(x->x ∈ all_inds ? image[x] : missing_val, selection .+ ind))
-         new_image[ind] = algo[index+1]
+function enhance(image, missing_val) algo[matbin2dec.(map.(x->x ∈ all_inds ? image[x] : missing_val, selections)).+1] end
+function execute(image, num)
+    for i in repeat([0,1], num÷2)
+        image = enhance(image, i)
     end
-    new_image
+    sum(image)
 end
 
-its = 50
-for i in repeat([0,1], its÷2)
-    global image = enhance(image, i)
-end
-println(sum(image))
+f = open("data/day20")
+const algo = map(x->x=='#' ? 1 : 0, collect(readline(f)))
+readline(f)
+
+image = stupidpad(mapreduce(x->map(y->y=='#' ? 1 : 0,collect(x)), hcat, readlines(f)), zeros, 51)
+const all_inds = CartesianIndices(image)
+const selections = [CartesianIndices((-1:1, -1:1)) .+ x for x in all_inds]
+println(execute(image, 50))
