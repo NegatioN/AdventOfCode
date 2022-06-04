268c0e6aa4a75ac142dfa626746a2a9a98fdb4bd
NegatioN <joakimrishaug@gmail.com>
2022-06-04 12:57:14 +0200

    day2 part2

--- 2021/rust/day2/src/main.rs
+++ 2021/rust/day2/src/main.rs
@@ -1,23 +1,33 @@
 use std::fs;
 
+fn calculate(input: &Vec<Vec<&str>>) -> (i32, i32, i32) {
+    let mut horiz:i32 = 0;
+    let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
+    for x in input {
+        let dist: i32 = x[1].parse().expect("Bug");
+        match x[0] {
+            "forward" => {
+                horiz += dist;
+                depth += (aim * dist)
+            }
+            "down" => aim += dist,
+            "up" => aim -= dist,
+            _ => println!("nothing"),
+        }
+    }
+    (horiz, depth, aim)
+}
 fn main() {
     let filename = "../../data/day2";
     let content = fs::read_to_string(filename).expect("Something went wrong");
-    let input: Vec<Vec<&str>> =  content
+    let input: Vec<Vec<&str>> = content
         .lines()
         .map(|n| n.split(" ").collect::<Vec<&str>>())
         .collect();
 
-    let mut horiz = 0;
-    let mut depth = 0;
-    for x in input{
-        let dist = x[1].parse::<u32>().expect("Bug");
-        match x[0] {
-            "forward" =>horiz+=dist,
-            "down" =>depth+=dist,
-            "up" =>depth-=dist,
-            _=>println!("nothing")
-        }
-    }
-    println!("{}", horiz*depth);
+    let res = calculate(&input);
+    println!("{}", res.0*res.2);
+    let res = calculate(&input);
+    println!("{}", res.0*res.1);
 }
