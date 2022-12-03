c2584107955dae37f78acd4c03133377c5c42246
NegatioN <joakimrishaug@gmail.com>
2022-12-03 12:31:42 +0100

    Apply set in one place

--- 2022/day3.py
+++ 2022/day3.py
@@ -1,10 +1,11 @@
 with open('data/day3', 'r') as f:
     data = f.read().strip().split('\n')
 
-def overlap_char(sets): return set.intersection(*sets).pop()
-def score_func(c): return ord(c) - 38 if c.upper() == c else ord(c) - 96
-def split_equal(s): return set(s[:len(s)/2]), set(s[len(s)/2:])
+def overlap_char(data): return set.intersection(*map(set, data)).pop()
+def score(c): return ord(c) - 38 if c.upper() == c else ord(c) - 96
+def split_equal(s): return s[:len(s)/2], s[len(s)/2:]
 
-print(sum(map(score_func, map(overlap_char, map(split_equal, data)))))
-def split_groups(data): return [[set(s) for s in data[i:i+3]] for i in range(0, len(data), 3)]
-print(sum(map(score_func, map(overlap_char, split_groups(data)))))
+print(sum(map(score, map(overlap_char, map(split_equal, data))))) # Part1
+
+def split_groups(data): return [data[i:i+3] for i in range(0, len(data), 3)]
+print(sum(map(score, map(overlap_char, split_groups(data))))) # Part2
