98ae4ce8363189f5389abb4e256e8dada6217d84
NegatioN <joakimrishaug@gmail.com>
2022-12-04 11:42:25 +0100

    use superset function instead of checking intersection

--- 2022/day4.py
+++ 2022/day4.py
@@ -4,5 +4,5 @@ with open('data/day4', 'r') as f:
 def toset(x): return set(range(x[0], x[1]+1))
 def compute(data, condition_f): return sum(map(lambda x: condition_f(*map(toset,x)), data))
 
-print(compute(data, lambda x,y: len(x & y) == len(x) or len(x & y) == len(y))) # Part1
+print(compute(data, lambda x,y: x.issuperset(y) or y.issuperset(x))) # Part1
 print(compute(data, lambda x, y: len(x & y) > 0)) # Part2
