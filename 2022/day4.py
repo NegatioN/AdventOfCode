5f4f7f90c148d70eb3cc2f74d1b015a3876d786a
NegatioN <joakimrishaug@gmail.com>
2022-12-04 11:38:31 +0100

    Shorten the code for day4 a bit

--- 2022/day4.py
+++ 2022/day4.py
@@ -2,13 +2,7 @@ with open('data/day4', 'r') as f:
     data = [[map(int, y.split('-')) for y in x.strip().split(',')] for x in f.readlines()]
 
 def toset(x): return set(range(x[0], x[1]+1))
-def part1_condition(x, y): return len(x & y) == len(x) or len(x & y) == len(y)
-def compute(data, condition_f):
-    count = 0
-    for x, y in data:
-        if condition_f(toset(x), toset(y)):
-            count += 1
-    return count
+def compute(data, condition_f): return sum(map(lambda x: condition_f(*map(toset,x)), data))
 
-print(compute(data, part1_condition)) # Part1
+print(compute(data, lambda x,y: len(x & y) == len(x) or len(x & y) == len(y))) # Part1
 print(compute(data, lambda x, y: len(x & y) > 0)) # Part2
