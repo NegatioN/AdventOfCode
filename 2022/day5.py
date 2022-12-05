c8fa3264c55381551181b79a2cd178c07a00af93
Joakim Rishaug <joakimrishaug@gmail.com>
2022-12-05 10:56:46 +0100

    Equally dirty solution?

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,33 +1,30 @@
 from copy import deepcopy
-
 with open('data/day5', 'r') as f:
     stacks, instructions, = f.read().split('\n\n')
+moves = []
+for line in map(lambda x: x.split(), instructions.strip().split('\n')):
+    num, fro, to = list(map(int, [line[1], line[3], line[5]]))
+    moves.append((num, fro-1, to-1))
 
 arrs = []
 for x in zip(*stacks.split('\n')):
     try:
-        int(x[-1])
+        int(x[-1]) # parse all lines with a number at the end
         arrs.append(list(filter(lambda x: x != ' ', x[::-1][1:])))
     except:
         pass
 
-
-def part1(arrs):
+def compute(arrs, moves, f):
     arrs = deepcopy(arrs)
-    for line in map(lambda x: x.split(), instructions.strip().split('\n')):
-        num, fro, to = list(map(int, [line[1], line[3], line[5]]))
-        for _ in range(num):
-            arrs[to-1].append(arrs[fro-1].pop())
-    return ''.join([x[-1] for x in arrs])
-
-def part2(arrs):
-    arrs = deepcopy(arrs)
-    for line in map(lambda x: x.split(), instructions.strip().split('\n')):
-        num, fro, to = list(map(int, [line[1], line[3], line[5]]))
-        arrs[to-1].extend(arrs[fro-1][-num:])
-        arrs[fro-1] = arrs[fro-1][:-num]
+    for num, fro, to in moves:
+        f(arrs, num, fro, to)
 
     return ''.join([x[-1] for x in arrs])
 
-print(part1(arrs))
-print(part2(arrs))
+def part1_f(arrs, num, fro, to):
+    for _ in range(num):
+        arrs[to].append(arrs[fro].pop())
+def part2_f(arrs, num, fro, to): arrs[to].extend(arrs[fro][-num:]); arrs[fro] = arrs[fro][:-num]
+
+print(compute(arrs, moves, part1_f))
+print(compute(arrs, moves, part2_f))
