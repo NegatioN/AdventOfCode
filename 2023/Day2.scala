29c23fd4c2ac3cfeb240d0a0233ac40431625f20
NegatioN <joakimrishaug@gmail.com>
2023-12-02 17:33:00 +0100

    d2p2

--- 2023/Day2.scala
+++ 2023/Day2.scala
@@ -32,6 +32,23 @@ object Day2 extends App {
     games.map{ case (key, value) => if(validGame(value, currentRules)) key else 0 }.sum
   }
 
-  println(part1(fname))
+  //println(part1(fname))
+
+  def power(n: Int): Int = n * n
+  def fewestPossible(games: Seq[Map[String, Int]]) = {
+    val keys: Seq[String] = Seq("red", "green", "blue")
+    val out = keys.map(k => games.map(g => g.getOrElse(k, 1)))
+    out.map(_.max).reduce(_*_)
+  }
+
+  def part2(fname: String) = {
+    val games = Source.fromFile(fname).getLines().map(line => {
+      parseExample(line)
+    }).toMap
+
+    games.map { case (key, value) => fewestPossible(value) }.sum
+
+  }
+  println(part2(fname))
 }
 
