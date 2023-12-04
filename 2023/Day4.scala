d919009fbbdae43970527933f4769e20c1f383e7
Joakim Rishaug <joakimrishaug@gmail.com>
2023-12-04 14:04:32 +0100

    tmp

--- 2023/Day4.scala
+++ 2023/Day4.scala
@@ -2,11 +2,16 @@ import scala.io.Source
 import scala.math.pow
 
 object Day4 extends App {
-  val fname = "data/day4"
+  val fname = "data/day4test"
   val text = Source.fromFile(fname).getLines().toSeq
 
   case class CardGame(num: Int, winningNums: Set[Int], myNums: Set[Int]){
     def score = pow(2, myNums.intersect(winningNums).size-1).intValue
+    def cardCopies = {
+      val overlap = myNums.intersect(winningNums).size
+      val res = Range(num+1, num+overlap+1).toSeq
+      res
+    }
   }
   def parse(line: String): CardGame = {
     val s"$num: $winningNums | $myNums" = line
@@ -21,4 +26,15 @@ object Day4 extends App {
 
   val a = part1(fname)
   println(a)
+
+  def part2(fname: String) = {
+    val games= Source.fromFile(fname).getLines().map(line => {
+      parse(line)
+    }).map(_.cardCopies).flatten.toSeq
+    println(games)
+    games.groupBy(a => a).mapValues(_.size).toSeq
+  }
+
+  val b = part2(fname)
+  println(b)
 }
